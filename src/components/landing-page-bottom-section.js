2663667ff2cbd6d1836367ffe92451fb84ae45eb
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-23 19:58:56 -0400

    add coinmarketcap widget

--- src/components/landing-page-bottom-section.js
+++ src/components/landing-page-bottom-section.js
@@ -10,7 +10,6 @@ export function LandingPageBottomSection () {
           important info you need to know.
         </div>
         <div className="watchlist-title">Watchlist</div>
-        <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script>
         <div className="coinmarketcap-currency-widget" data-currencyid="1" data-base="USD" data-secondary=""
              data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
              data-statsticker="false"></div>
