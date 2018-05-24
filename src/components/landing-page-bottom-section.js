c209a724e4491f64d033f5f61cd6dfe63278c4da
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-23 20:06:19 -0400

    finish landing page bottom section

--- src/components/landing-page-bottom-section.js
+++ src/components/landing-page-bottom-section.js
@@ -10,9 +10,26 @@ export function LandingPageBottomSection () {
           important info you need to know.
         </div>
         <div className="watchlist-title">Watchlist</div>
-        <div className="coinmarketcap-currency-widget" data-currencyid="1" data-base="USD" data-secondary=""
-             data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
-             data-statsticker="false"></div>
+        <div className="watchlist-grid">
+          <div className="coinmarketcap-currency-widget" data-currencyid="1" data-base="USD" data-secondary=""
+               data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
+               data-statsticker="false"></div>
+          <div className="coinmarketcap-currency-widget" data-currencyid="1027" data-base="USD" data-secondary=""
+               data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
+               data-statsticker="false"></div>
+          <div className="coinmarketcap-currency-widget" data-currencyid="1230" data-base="USD" data-secondary=""
+               data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
+               data-statsticker="false"></div>
+          <div className="coinmarketcap-currency-widget" data-currencyid="1765" data-base="USD" data-secondary=""
+               data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
+               data-statsticker="false"></div>
+          <div className="coinmarketcap-currency-widget" data-currencyid="1376" data-base="USD" data-secondary=""
+               data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
+               data-statsticker="false"></div>
+          <div className="coinmarketcap-currency-widget" data-currencyid="1839" data-base="USD" data-secondary=""
+               data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD"
+               data-statsticker="false"></div>
+        </div>
       </div>
     </div>
   )
