5d0b89248779b379d66553d51069f0cea9c6e7a5
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-24 12:40:58 -0400

    finish tweaking pie chart to allow for assigning color to each slice

--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -9,29 +9,37 @@ export class PieChart extends Component {
 
   render () {
     const {value} = this.state
+
     const purple = '#A084D1'
     const pink = '#F934AA'
     const blue = '#5EA3E7'
     const orange = '#FFAA33'
+    const darkGray = '#4A4A4A'
+
+    const pieSliceHover =
+      <Hint value={value}>
+        <div className="pie-slice-hover" style={{background: 'white'}}>
+          <div className="pie-slice-hover-text">{value.symbol} ({value.percentage}%)</div>
+        </div>
+      </Hint>
 
     return (
       <RadialChart
         className={'pie-chart'}
-        // innerRadius={100}
-        colorRange={[purple, pink, blue, orange]}
+        // colorRange={[purple, pink, blue, orange]}
         radius={200}
         getAngle={d => d.theta}
         data={[
-          {theta: 2, label: 'BTC', className: 'pie-slice'},
-          {theta: 6, className: 'pie-slice'},
-          {theta: 2, className: 'pie-slice'},
-          {theta: 3, className: 'pie-slice'},
+          {theta: 2, symbol: 'BTC', percentage: 25, style: {fill: blue, stroke: darkGray, strokeWidth: 3}},
+          {theta: 4, symbol: 'BTC', percentage: 25, style: {fill: pink, stroke: darkGray, strokeWidth: 3}},
+          {theta: 2, symbol: 'BTC', percentage: 25, style: {fill: purple, stroke: darkGray, strokeWidth: 3}},
+          {theta: 2, symbol: 'BTC', percentage: 25, style: {fill: orange, stroke: darkGray, strokeWidth: 3}},
         ]}
         onValueMouseOver={v => this.setState({value: v})}
         onSeriesMouseOut={() => this.setState({value: false})}
         width={400}
         height={400}>
-        {value && <Hint value={value}/>}
+        {value && pieSliceHover}
       </RadialChart>
     )
   }
