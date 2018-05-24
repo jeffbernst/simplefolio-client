19f35b323a8efb8f11674f62abcf47f3e5f4f6ea
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-24 18:54:30 -0400

    render pie chart from sample portfolio data and remove hover over effects

--- src/components/landing-page-middle-section.js
+++ src/components/landing-page-middle-section.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './landing-page-middle-section.css'
 import { PortfolioEntry } from './portfolio-entry'
 import { PieChart } from './pie-chart'
-import { colors } from '../colors'
+import { colors, darkGray } from '../colors'
 
 const samplePortfolio = [
   {
@@ -45,17 +45,24 @@ export class LandingPageMiddleSection extends React.Component {
 
   render () {
     let portfolioList
-
-    if (this.state.cryptoPriceData) {
+    let pieChartData = []
+    if (this.state.cryptoPriceData)
       portfolioList = samplePortfolio.map((item, index) => {
+        // iterate through portfolio holdings and match up with current price data
         const itemData = this.state.cryptoPriceData[item.id]
         const {name, symbol} = itemData
         const price = itemData.quotes.USD.price.toFixed(2)
-        const priceWithCommas = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        const priceWithCommas = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
         const percentChange = itemData.quotes.USD.percent_change_24h
         const {quantity} = item
-        const balance = quantity * price
+        const balance = (quantity * price).toFixed(2)
         const colorIndex = index <= colors.length ? index : index % colors.length
+
+        pieChartData.push({
+          theta: balance,
+          symbol,
+          style: {fill: colors[colorIndex], stroke: darkGray, strokeWidth: 3}
+        })
 
         return (
           <PortfolioEntry
@@ -70,7 +77,6 @@ export class LandingPageMiddleSection extends React.Component {
           />
         )
       })
-    }
 
     return (
       <div className="background-middle">
@@ -85,7 +91,7 @@ export class LandingPageMiddleSection extends React.Component {
               {portfolioList}
             </div>
             <div className="pie-chart">
-              <PieChart/>
+              <PieChart pieChartData={pieChartData}/>
             </div>
           </div>
         </div>
--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -1,46 +1,16 @@
-import React, { Component } from 'react'
-import { RadialChart, Hint } from 'react-vis'
+import React from 'react'
+import { RadialChart } from 'react-vis'
 import './pie-chart.css'
 
-export class PieChart extends Component {
-  state = {
-    value: false
-  }
-
-  render () {
-    const {value} = this.state
-
-    const purple = '#A084D1'
-    const pink = '#F934AA'
-    const blue = '#5EA3E7'
-    const orange = '#FFAA33'
-    const darkGray = '#4A4A4A'
-
-    const pieSliceHover =
-      <Hint value={value}>
-        <div className="pie-slice-hover" style={{background: 'white'}}>
-          <div className="pie-slice-hover-text">{value.symbol} ({value.percentage}%)</div>
-        </div>
-      </Hint>
-
-    return (
-      <RadialChart
-        className={'pie-chart'}
-        // colorRange={[purple, pink, blue, orange]}
-        radius={200}
-        getAngle={d => d.theta}
-        data={[
-          {theta: 2, symbol: 'BTC', percentage: 25, style: {fill: blue, stroke: darkGray, strokeWidth: 3}, className: 'pie-slice'},
-          {theta: 4, symbol: 'BTC', percentage: 25, style: {fill: pink, stroke: darkGray, strokeWidth: 3}, className: 'pie-slice'},
-          {theta: 2, symbol: 'BTC', percentage: 25, style: {fill: purple, stroke: darkGray, strokeWidth: 3}, className: 'pie-slice'},
-          {theta: 2, symbol: 'BTC', percentage: 25, style: {fill: orange, stroke: darkGray, strokeWidth: 3}, className: 'pie-slice'},
-        ]}
-        onValueMouseOver={v => this.setState({value: v})}
-        onSeriesMouseOut={() => this.setState({value: false})}
-        width={400}
-        height={400}>
-        {value && pieSliceHover}
-      </RadialChart>
-    )
-  }
+export function PieChart (props) {
+  return (
+    <RadialChart
+      className={'pie-chart'}
+      radius={200}
+      getAngle={d => d.theta}
+      data={props.pieChartData}
+      width={400}
+      height={400}>
+    </RadialChart>
+  )
 }
