9fb8b403394a0db9e35c8a656bf973443190662b
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-25 11:37:26 -0400

    finish styling login page

--- src/components/login.js
+++ src/components/login.js
@@ -9,16 +9,14 @@ export function Login () {
         <LandingNav/>
         <form className="login-form">
           <div className="login-message">Log in to go to your account. <span role='img' aria-label='slightly smiling emoji'>&#x1F642;</span></div>
-          <div className="modal-form-field">
-            <label htmlFor="login-email">Email: </label>
-            <input type="text" className="login-email input-box modal-input-box" id="login-email" required />
+          <div className="login-field">
+            <input type="text" className="login-email" id="login-email" placeholder="email" required />
           </div>
-          <div className="modal-form-field">
-            <label htmlFor="login-password">Password: </label>
-            <input type="password" className="login-email input-box modal-input-box" id="login-password" required />
+          <div className="login-field">
+            <input type="password" className="login-email" id="login-password" placeholder="password" required />
           </div>
-          <div className="modal-submit-button-container">
-            <button type="submit" className='login-submit-button slim-button modal-submit-button'>Log In</button>
+          <div className="submit-button-container">
+            <button type="submit" className='login-submit-button fade-in-out'>Let's go!</button>
           </div>
         </form>
       </div>
