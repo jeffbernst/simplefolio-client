26a66d315d6c00d1faca72b5b3f8783512b6366c
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-30 13:39:31 -0400

    begin converting singup form to redux form

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,28 +1,63 @@
 import React from 'react'
-import {LandingNav} from './landing-nav'
+import { LandingNav } from './landing-nav'
 import './sign-up.css'
+import { Field, reduxForm, focus } from 'redux-form'
+import Input from './input'
+// import { registerUser } from '../actions/users'
+// import { login } from '../actions/auth'
+import { required, nonEmpty, matches, length, isTrimmed } from '../validators'
 
-export function SignUp () {
-  return (
-    <div className="login-signup-background">
-      <div className="container">
-        <LandingNav/>
-        <form className="signup-form">
-          <div className="login-signup-message">Create an account to get started!</div>
-          <div className="login-signup-field">
-            <input type="text" className="signup-email" id="signup-email" placeholder="email" required />
-          </div>
-          <div className="login-signup-field">
-            <input type="password" className="signup-password" id="signup-password" placeholder="password" required />
-          </div>
-          <div className="login-signup-field">
-            <input type="password" className="signup-email-confirmation" id="signup-password-confirmation" placeholder="confirm password" required />
-          </div>
-          <div className="submit-button-container">
-            <button type="submit" className='login-signup-submit-button fade-in-out'>Let's go!</button>
-          </div>
-        </form>
+const passwordLength = length({min: 10, max: 72})
+const matchesPassword = matches('password')
+
+class SignUp extends React.Component {
+  onSubmit (values) {
+    // const {username, password, passwordConfirmation} = values
+    // const user = {username, password, firstName, lastName}
+    // return this.props
+    //   .dispatch(registerUser(user))
+    //   .then(() => this.props.dispatch(login(username, password)))
+    console.log(values)
+  }
+
+  render () {
+    return (
+      <div className="login-signup-background">
+        <div className="container">
+          <LandingNav/>
+          <form className="signup-form"
+                onSubmit={this.props.handleSubmit(values =>
+                  this.onSubmit(values)
+                )}>
+            <div className="login-signup-message">Create an account to get started!</div>
+            <div className="login-signup-field">
+              <Field component={Input} type="text" name="username" className="signup-username" placeholder="username"
+                     validate={[required, nonEmpty, isTrimmed]}/>
+            </div>
+            <div className="login-signup-field">
+              <Field component={Input} type="password" name="password" className="signup-password"
+                     placeholder="password" validate={[required, passwordLength, isTrimmed]}/>
+            </div>
+            <div className="login-signup-field">
+              <Field component={Input} type="password" name="passwordConfirmation"
+                     className="signup-password-confirmation" placeholder="confirm password"
+                     validate={[required, nonEmpty, matchesPassword]}/>
+            </div>
+            <div className="submit-button-container">
+              <button type="submit" className='login-signup-submit-button fade-in-out'
+                      disabled={this.props.pristine || this.props.submitting}>
+                Let's go!
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default reduxForm({
+  form: 'registration',
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus('registration', Object.keys(errors)[0]))
+})(SignUp)
