cf27014fc380fc1e2cb4bde084006728cdac9163
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-31 15:23:27 -0400

    add config file

--- src/actions/protected-data.js
+++ src/actions/protected-data.js
@@ -1,4 +1,4 @@
-// import {API_BASE_URL} from '../config';
+import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
 
 export const FETCH_PROTECTED_DATA_SUCCESS = 'FETCH_PROTECTED_DATA_SUCCESS';
