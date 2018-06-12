316cea09d24192338c51f42fa580d5b2a46c03dd
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-06-12 19:15:57 -0400

    add some tests back in so travis will pass

--- src/tests/actions/index.test.js
+++ src/tests/actions/index.test.js
@@ -1,22 +1,22 @@
-// import { getPriceDataRequest, getPriceDataSuccess, getPriceDataError } from '../../actions'
-// import { GET_PRICE_DATA_REQUEST, GET_PRICE_DATA_SUCCES, GET_PRICE_DATA_ERROR } from '../../actions/types'
-//
-// describe('getPriceDataRequest', () => {
-//   it('Should return the action', () => {
-//     const action = getPriceDataRequest()
-//     expect(action.type).toEqual(GET_PRICE_DATA_REQUEST)
-//   })
-// })
-//
-// describe('getPriceDataSuccess', () => {
-//   it('Should return the action', () => {
-//     const priceData = {prices: 'lots of prices!'}
-//     const action = getPriceDataSuccess(priceData)
-//     expect(action.type).toEqual(GET_PRICE_DATA_SUCCES)
-//     expect(action.payload).toEqual(priceData)
-//   })
-// })
-//
+import { getPriceDataRequest, getPriceDataSuccess, getPriceDataError } from '../../actions'
+import { GET_PRICE_DATA_REQUEST, GET_PRICE_DATA_SUCCES, GET_PRICE_DATA_ERROR } from '../../actions/types'
+
+describe('getPriceDataRequest', () => {
+  it('Should return the action', () => {
+    const action = getPriceDataRequest()
+    expect(action.type).toEqual(GET_PRICE_DATA_REQUEST)
+  })
+})
+
+describe('getPriceDataSuccess', () => {
+  it('Should return the action', () => {
+    const priceData = {prices: 'lots of prices!'}
+    const action = getPriceDataSuccess(priceData)
+    expect(action.type).toEqual(GET_PRICE_DATA_SUCCES)
+    expect(action.payload).toEqual(priceData)
+  })
+})
+
 // describe('getPriceDataError', () => {
 //   it('Should return the action', () => {
 //     const error = 'my error'
