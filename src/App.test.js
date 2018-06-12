cf63d67b2d106a87bb190964732b49c43a212fc0
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-06-12 19:28:03 -0400

    wrap app test in provider tags

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
+import store from './store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Router><App /></Router>, div);
+  ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
