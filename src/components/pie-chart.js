97403e1e83317b7e97cb8c1e39f66d0f5209ec10
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-06-15 13:54:24 -0400

    responsive updates for portfolio entries and pie chart

--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -3,14 +3,27 @@ import { RadialChart } from 'react-vis'
 import './pie-chart.css'
 
 export function PieChart (props) {
+  // return multiple pie charts and show
+  // proper size based on window size
+  // with CSS
   return (
-    <RadialChart
-      data={props.pieChartData}
-      className={'pie-chart'}
-      radius={200}
-      getAngle={d => d.theta}
-      width={400}
-      height={400}>
-    </RadialChart>
+    <div>
+      <RadialChart
+        data={props.pieChartData}
+        className={'pie-chart pie-chart-small'}
+        radius={150}
+        getAngle={d => d.theta}
+        width={300}
+        height={300}>
+      </RadialChart>
+      <RadialChart
+        data={props.pieChartData}
+        className={'pie-chart-large'}
+        radius={200}
+        getAngle={d => d.theta}
+        width={400}
+        height={400}>
+      </RadialChart>
+    </div>
   )
 }
